b0768139ba10eb036c633add915ffb90d0c3e39b
aliranaofficial <joinalirana@gmail.com>
2023-08-21 11:23:21 -0700

    loopes practice

--- class05/index.js
+++ class05/index.js
@@ -36,17 +36,45 @@ for(let i=1; i<=100; i++){
 
 }
 console.log("sum",sum);
-*/
+
+
 //wrte a ts program that calculates even number and also tje list
-let sum = 0;
+
+
+
+
+let sum:number =0;
+let list:number[]=[]
+for(let i=1; i<=100; i++){
+    
+    if (i%2 == 0) {
+        sum=sum + i
+        list.push(i)
+       // console.log("i",i);
+    }
+
+}
+console.log("sum",sum, list);
+
+
+var num:number=69;
+console.log(num);
+*/
+for (let index = 0; index < 12; index++) {
+    console.log("alirana", index);
+}
+var index = 30;
+do {
+    console.log("ali", index);
+} while (index <= 13);
+let sum = 2;
 let list = [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 2 == 0) {
-        sum = sum + i;
-        list.push(i);
-        // console.log("i",i);
+for (let index = 2; index <= 300; index++) {
+    // console.log("index",index);
+    if (index % 2 !== 0) {
+        sum = sum + index;
+        list.push(index);
+        // console.log("num",index);
     }
 }
 console.log("sum", sum, list);
-var num = 69;
-console.log(num);
