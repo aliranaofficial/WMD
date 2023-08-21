f0c30749938c10bae3a63de43d7feedd652cccae
aliranaofficial <joinalirana@gmail.com>
2023-08-20 23:35:01 -0700

    if statement

--- class02/class/index.js
+++ class02/class/index.js
@@ -2,3 +2,7 @@
 console.log("Hello World");
 var num = 14;
 console.log(num);
+let sum = 4;
+if (sum < 2) {
+    console.log("number", sum);
+}
--- class02/class/index.ts
+++ class02/class/index.ts
@@ -3,3 +3,9 @@ console.log("Hello World");
 var num:number= 14;
 
 console.log(num);
+
+let sum:number=4;
+if (sum<2) {
+    console.log("number",sum)
+    
+}
