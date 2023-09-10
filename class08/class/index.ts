12ff61a823bac5b6ee66fbe040a5a085b375ebc8
aliranaofficial <joinalirana@gmail.com>
2023-09-10 05:49:58 -0700

    new concept

---
+++ class08/class/index.ts
@@ -0,0 +1,38 @@
+// set timeout
+console.log("Start");
+
+setTimeout(()=>{
+    console.log("in sttimeout");
+},5000);
+
+console.log("end");
+
+
+// set interval
+console.log("Start");
+
+function printInFunction(){
+    console.log("in setinterval");
+    count++;
+if (count==3){
+clearInterval(get)
+
+}
+    
+}
+let count=0;
+const get=setInterval(printInFunction,2000);
+
+
+console.log("end");
+
+
+
+
+
+
+
+
+
+
+
